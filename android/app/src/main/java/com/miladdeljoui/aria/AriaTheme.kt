4392eb146a60aed4122898108eac91f33154ab15
Miladdeljoui <136453100+Miladdeljoui@users.noreply.github.com>
2026-09-19 21:13:39 +0330

    ui: add ARIA visual theme tokens

---
+++ android/app/src/main/java/com/miladdeljoui/aria/AriaTheme.kt
@@ -0,0 +1,13 @@
+package com.miladdeljoui.aria
+
+import androidx.compose.ui.graphics.Color
+
+val AriaBackground = Color(0xFF05070A)
+val AriaPanel = Color(0xFF0B1016)
+val AriaPanelSoft = Color(0xFF101821)
+val AriaCyan = Color(0xFF3EE8FF)
+val AriaCyanSoft = Color(0xFF16363D)
+val AriaText = Color(0xFFEAF8FF)
+val AriaMuted = Color(0xFF8BA3AF)
+val AriaWarning = Color(0xFFFFC857)
+val AriaDanger = Color(0xFFFF667A)
